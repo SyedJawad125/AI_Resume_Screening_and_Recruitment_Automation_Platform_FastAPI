9c65ff6f97752f19386d78bc7e9bf6b7eecfb898
Syed Jawad Ali <syedjawadali92@gmail.com>
2026-09-07 11:52:00 +0500

    Changes

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,14 +1,3 @@
-# # init_db.py
-# from app.database import Base, engine
-# from app import models # ensure all models are imported
-# # from app.models.rank import Rank
-# # Create all tables
-# Base.metadata.create_all(bind=engine)
-# print("✅ Database tables created successfully.")
-
-
-
-
 """
 init_db.py
 ───────────
